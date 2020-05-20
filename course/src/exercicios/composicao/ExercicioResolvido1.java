6abf507b04aa77b3dea0fae76bc06f96f1fef246
Marta Schmitt <mcristina.schmitt@gmail.com>
2020-05-19 21:26:19 -0300

    Composicao - Exercicio resolvido 1

---
+++ course/src/exercicios/composicao/ExercicioResolvido1.java
@@ -0,0 +1,11 @@
+package exercicios.composicao;
+
+public class ExercicioResolvido1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
---
+++ course/src/exercicios/composicao/entities/enums/WorkerLevel.java
@@ -0,0 +1,9 @@
+package exercicios.composicao.entities.enums;
+
+public enum WorkerLevel {
+
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
+	
+}
