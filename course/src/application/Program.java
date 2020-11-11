ec4a500fd38e235b804cea7c116b4de31dfc44dc
Marta Schmitt <mcristina.schmitt@gmail.com>
2020-11-10 22:01:28 -0300

    Classes abstratas

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,21 +1,39 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		Locale.setDefault(Locale.US);
+		List<Account> list = new ArrayList<>();
 		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
+		list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 		
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+		double sum = 0.0;
+		for (Account account : list) {
+			sum += account.getBalance();
+		}
 		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for (Account account : list) {
+			account.deposit(10.0);
+		}
+		
+		for (Account account : list) {
+			System.out.printf("Updated balance for account %d: %.2f%n", account.getNumber(), account.getBalance());
+		}
 	}
 	
 }
