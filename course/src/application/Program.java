2d3b063fc281ade6d81a4c0141c6eac071509f4f
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-03-16 20:25:29 -0300

    Estrutura try-catch

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -17,36 +18,17 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		List<Shape> list = new ArrayList<>();
-		
-		System.out.print("Enter the numbre of shapes: ");
-		int n = sc.nextInt();
-		
-		for (int i = 1; i <= n; i++) {
-			System.out.println("Shape #" + i + " data:");
-			System.out.print("Rectangle or Circle (r/c)? ");
-			char ch = sc.next().charAt(0);
-			System.out.print("Color (BLACK/BLUE/RED): ");
-			Color color = Color.valueOf(sc.next());
-			if (ch == 'r') {
-				System.out.print("Width: ");
-				double width = sc.nextDouble();
-				System.out.print("Height: ");
-				double height = sc.nextDouble();
-				list.add(new Rectangle(color, width, height));
-			} else {
-				System.out.print("Radius: ");
-				double radius = sc.nextDouble();
-				list.add(new Circle(color, radius));
-			}
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		} catch (InputMismatchException e) {
+			System.out.println("Input error");
 		}
 		
-		System.out.println();
-		System.out.println("SHAPE AREAS:");
-		
-		for (Shape shape : list) {
-			System.out.println(String.format("%.2f", shape.area()));
-		}
+		System.out.println("End of program");
 		
 		sc.close();
 		
