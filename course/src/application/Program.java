4dca6a5076febcf3550b7d5b50d7d3a15ac0eb43
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-03-16 20:36:50 -0300

    Pilha de chamadas de metodos (stack trace)

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,21 +1,26 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Shape;
-import entities.Circle;
-import entities.Rectangle;
-import entities.enums.Color;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
+		method1();
+		
+		System.out.println("End of program");
+				
+	}
+
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
 		Scanner sc = new Scanner(System.in);
 		
 		try {
@@ -24,14 +29,14 @@ public class Program {
 			System.out.println(vect[position]);			
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
 		} catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
 		
-		System.out.println("End of program");
-		
 		sc.close();
-		
+		System.out.println("***METHOD2 END***");
 	}
 	
 }
